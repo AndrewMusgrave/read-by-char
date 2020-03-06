5dcfc34b7ea8e8cd03847993c71952c00936d3d3
Andrew Musgrave <andrew.musgrave@shopify.com>
2020-03-05 20:21:54 -0500

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,7 @@ declare function readByChar(
   path: string,
   onChar: readByChar.OnChar,
   onClose: () => void
-);
+): void;
 
 declare namespace readByChar {
   type OnChar = (character: string, column: number, line: number) => void;
